d42f539134372cce9a7cda4ce60762e6a7bc0397
Adamo198 <adamo123400@gmail.com>
2023-05-27 20:02:16 +0200

    backend progress

---
+++ README.md
@@ -0,0 +1,2 @@
+# Survey Application
+A microservice survey application done with Vue3, Spring Cloud and MongoDB.
---
+++ discovery-service/src/test/java/com/surveyapp/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surveyapp.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
