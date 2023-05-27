4f49b6a9bf47998c8f4e62a2fc2efd80abef3ba1
Adamo198 <adamo123400@gmail.com>
2023-05-27 19:28:27 +0200

    survey service added

---
+++ survey-service/src/main/java/com/surveyapp/surveyservice/documents/Option.java
@@ -0,0 +1,8 @@
+package com.surveyapp.surveyservice.documents;
+
+import lombok.Data;
+
+@Data
+public class Option {
+    private String text;
+}
---
+++ survey-service/src/main/java/com/surveyapp/surveyservice/documents/Survey.java
@@ -0,0 +1,24 @@
+package com.surveyapp.surveyservice.documents;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+/**
+ * Created by adamo198 on 26/05/2023.
+ * Github:adamo198
+ */
+@Document
+@Data
+public class Survey {
+
+    @Id
+    private String id;
+    private String title;
+    private String email;
+    private List<Question> questions;
+
+}
